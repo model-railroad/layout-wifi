d22615435777b59623f14c0e05687537ee810911
ralf <ralfoide@gmail.com>
2015-08-22 23:48:57 -0700

    Translate: basic http MJPEG stream decoder, basic http img debug server

--- translate/src/translate/cam_sensor.go
+++ translate/src/translate/cam_sensor.go
@@ -3,51 +3,157 @@ package translate
 import (
     "flag"
     "fmt"
-    "net"
+    "io"
+    "image"
+    "image/jpeg"
+    "mime/multipart"
+    "net/http"
+    net_url "net/url"
     "strings"
+    "sync"
     "time"
 )
 
-var CAM_HOSTS = flag.String("cam-hosts", "camera1.local:80,camera2.local:80", "Cameras host:port")
+var CAM_SERV = flag.String("cam-server", ":8088", "Camera debug server host:port")
 
-type Camera struct {
-    index   int
-    host    string
-}
+var CAM_HOSTS = flag.String("cam-urls",
+    "camera1.local:80/path.cgi?user=foo&pwd=blah," +
+    "user2:pwd2@camera2.local/path.cgi", 
+    "Cameras user:pwd@host:port/path")
 
+// Typical URLs:
+// Foscam: http://foscam.ip/videostream.cgi?resolution=32&user=USER&pwd=PWD
+// Edimax: http://USER:PWD@edimax.ip/mjpg/video.mjpg
 
 //-----
 
+var CAMERAS []*Camera
+
+type Camera struct {
+    index   int
+    url     *net_url.URL
+    mutex   sync.Mutex
+    last    image.Image
+}
+
+func NewCamera(index int, url_str string) *Camera {
+    u, err := net_url.Parse(url_str)
+    if err != nil {
+        panic(err)
+    }
+    cam := &Camera{}
+    cam.index = index
+    cam.url = u
+    return cam
+}
+
+//-----
+
+func CamSensorDebugServer() {
+    http.HandleFunc("/debug", CamSensorDebugHandler)
+
+    if err := http.ListenAndServe(*CAM_SERV, nil /*handler*/); err != nil {
+        panic(err)
+    }
+}
+
+func CamSensorDebugHandler(w http.ResponseWriter, req *http.Request) {
+    if len(CAMERAS) > 0 {
+        cam := CAMERAS[0]
+        img := cam.GetLast()
+        if img != nil {
+            w.Header().Set("Content-Type", "image/jpeg")
+            w.WriteHeader(http.StatusOK)
+            jpeg.Encode(w, img, nil /*Options*/)
+            return
+        }
+    }
+    
+    w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+    w.WriteHeader(http.StatusOK)
+    io.WriteString(w, "No image available.\n")
+}
+
+//-----
 
 func CamSensorClient(m *Model) {
-    fmt.Println("Start cam-sensor client")
+    fmt.Println("Start Cam-Sensor client")
 
-    hosts := strings.Split(*CAM_HOSTS, ",")
+    urls := strings.Split(*CAM_HOSTS, ",")
 
-    for index, host := range hosts {
-        cam := &Camera { index, host }
+    for index, url := range urls {
+        cam := NewCamera(index, url)
+        CAMERAS = append(CAMERAS, cam)
 
         go func(cam *Camera) {
             for !m.IsQuitting() {
-                fmt.Printf("[CAM %d] connecting to server %s\n", cam.index, cam.host)
-                conn, err := net.Dial("tcp", cam.host)
+                fmt.Printf("[CAM %d] connecting to server %s\n", cam.index, cam.url.Host)
+
+                /*
+                req, err := http.NewRequest("GET", cam.url, nil)
                 if err != nil {
-                    fmt.Printf("[CAM %d] READ Connection error: %v", cam.index, err)
+                    if cam.url.User != nil {
+                        req.SetBasicAuth(cam.url.User.Username(), cam.url.User.Password())
+                    }
+                    client := &http.Client{}
+                    resp, err := client.Do(req)
+                }
+                */
+                resp, err := http.Get(cam.url.String())
+
+                if err != nil {
+                    fmt.Printf("[CAM %d: %s] READ Connection error: %v\n", cam.index, cam.url.Host, err)
                     time.Sleep(5 * time.Second)
 
                 } else {
-                    cam.CamClient(conn, m)
+                    cam.CamClient(resp.Body, m)
                 }
             }
         }(cam)
     }
-
 }
 
-func (cam *Camera) CamClient(conn net.Conn, m *Model) {
-    fmt.Println("[LWC-READER] New READ connection")
-    defer conn.Close()
+func (cam *Camera) CamClient(stream io.ReadCloser, m *Model) {
+    fmt.Printf("[CAM %d: %s] New READ connection\n", cam.index, cam.url.Host)
+    
+    boundary := "ipcamera" // TODO
+    
+    defer stream.Close()
+    reader := multipart.NewReader(stream, boundary)
+    
     loopRead: for !m.IsQuitting() {
-        break loopRead
+        part, err := reader.NextPart()
+        if err == io.EOF {
+            break loopRead
+        } else if err != nil {
+            panic(err)
+        }
+        
+        //-- bytes, err := ioutil.ReadAll(part)
+        defer part.Close()
+        
+        img, err := jpeg.Decode(part)
+        if err != nil {
+            panic(err)
+        }
+        
+        cam.SetLast(img)
+        
+        fmt.Printf("[CAM %d: %s] Decoded JPEG %d x %d\n", 
+            cam.index, cam.url.Host, 
+            img.Bounds().Dx(), img.Bounds().Dy())
     }
+    fmt.Printf("[CAM %d: %s] READ Connection closed\n", cam.index, cam.url.Host)
+}
+
+func (cam *Camera) GetLast() image.Image {
+    cam.mutex.Lock()
+    defer cam.mutex.Unlock()
+    return cam.last
+}
+
+func (cam *Camera) SetLast(img image.Image) {
+    cam.mutex.Lock()
+    defer cam.mutex.Unlock()
+    cam.last = img
 }
